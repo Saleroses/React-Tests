db36f73911db9e851f0427982f2a3ef33209d1b0
Егор <saleroses@yandex.ru>
2023-03-17 00:44:05 +0300

    map

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Switcher from "./Components/Switcher/Switcher";
+import Cars from "./Components/Filter/Filter";
+import NormCars from "./Components/Filter/Filter";
+import CarsFiltered from "./Components/Filter/Filter";
 
 function App() {
     return (
         <div className="App">
-            <Switcher active={true}/>
+            {/*<Switcher active={true}/>*/}
+            <CarsFiltered />
         </div>
     );
 }
