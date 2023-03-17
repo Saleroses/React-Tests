99ef7df7720f2f380cdf83c7f95f2aa33f85774e
Егор <saleroses@yandex.ru>
2023-03-17 19:55:20 +0300

    input

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Switcher from "./Components/Switcher/Switcher";
 import {Input} from "./Components/Input/Input";
+import {GetNames} from "./Components/JS Native/JS-1";
 
 function App() {
     return (
         <div className="App">
             {/*<Switcher active={true}/>*/}
-            <Input/>
+            {/*<Input/>*/}
         </div>
     );
 }
